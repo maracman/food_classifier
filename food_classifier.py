aa42aaa69fdf0fa23d9ecd08a24298204c568410
maracman <82516491+maracman@users.noreply.github.com>
2021-09-08 01:55:08 +1000

    Update food_classifier.py

--- food_classifier.py
+++ food_classifier.py
@@ -8,25 +8,11 @@ from fastai.imports import *
 path = Path()
 path.ls(file_exts='.pkl')
 learn_inf = load_learner(path/'export.pkl')
-
 btn_upload = widgets.FileUpload()
-btn_upload
-
-img = PILImage.create(btn_upload.data[-1])
-
 out_pl = widgets.Output()
-out_pl.clear_output()
-with out_pl: display(img.to_thumb(128,128))
-out_pl
-
 pred,pred_idx,probs = learn_inf.predict(img)
-
 lbl_pred = widgets.Label()
-lbl_pred.value = f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}'
-lbl_pred
-
 btn_run = widgets.Button(description='Classify')
-btn_run
 
 def on_click_classify(change):
     img = PILImage.create(btn_upload.data[-1])
